555315c904d90bdcb4aad2b42af9ded474ac859f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-01 17:04:45 +0300

    register controller with response resources

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PrivateUserResource;
+use App\Http\Requests\Auth\RegisterRequest;
 
 class RegisterController extends Controller
 {
-    //
+    public function action(RegisterRequest $request)
+    {
+        $user = User::create($request->only('email', 'name', 'password'));
+
+        return new PrivateUserResource($user);
+    }
 }
