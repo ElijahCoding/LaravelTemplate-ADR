07c62e881fdc6a10af679548615ca8c3e4f0db65
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-01 16:33:29 +0300

    preparing for tests

---
+++ tests/Unit/Region/RegionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Region;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class \RegionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
