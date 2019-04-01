7c4a10680ff5d78e1bcff8f7a4c25853409caa04
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-01 17:39:38 +0300

    login a user

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'Auth\LoginController@action');
     Route::get('me', 'Auth\MeController@action');
 });
+
+Route::group(['middleware' => 'auth:api'], function () {
+
+});
