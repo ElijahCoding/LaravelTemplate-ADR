a65e78a625fca045f4762fc72dbbcf75105f7a55
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-12 12:52:35 +0300

    fetching vehicles data

---
+++ app/Console/Commands/Vehicle/GetVehicleCategories.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands\Vehicle;
+
+use Illuminate\Console\Command;
+
+class GetVehicleCategories extends Command
+{
+    protected $signature = 'get:vehicle-categories';
+
+    protected $description = 'Get vehicle categories';
+
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/Console/Commands/Vehicle/GetVehicleTypes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands\Vehicle;
+
+use Illuminate\Console\Command;
+
+class GetVehicleTypes extends Command
+{
+    protected $signature = 'get:vehicle-types';
+
+    protected $description = 'Get vehicle types';
+
+    public function handle()
+    {
+        //
+    }
+}
