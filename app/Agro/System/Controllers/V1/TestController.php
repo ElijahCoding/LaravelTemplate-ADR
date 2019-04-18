2b9c27b3b2359defdd4deec50ada92f349ad0f8d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-18 15:53:16 +0300

    contracts built refs:#309

--- app/Agro/System/Controllers/V1/TestController.php
+++ app/Agro/System/Controllers/V1/TestController.php
@@ -2,7 +2,7 @@
 
 namespace App\Agro\System\Controllers\V1;
 
-use App\Http\Controllers\Controller;
+use App\Common\Controllers\Controller;
 
 class TestController extends Controller
 {
---
+++ app/Common/Models/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Common\Models;
+
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+class Model extends BaseModel
+{
+    
+}
