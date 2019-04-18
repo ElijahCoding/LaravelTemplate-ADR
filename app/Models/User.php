ca71f964d81d1eb76674a8ebe7b6f8ceb36c18ee
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-18 16:47:18 +0300

    removing , continue removing

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,6 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Models\Permissions\{
-    Role, Permission, Domain
-};
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -21,29 +18,4 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function domains()
-    {
-        return $this->belongsToMany(Domain::class, 'user_domain');
-    }
-
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
-
-    public function permissions()
-    {
-        return $this->belongsToMany(Permission::class, 'user_permission');
-    }
-
-    public function getJWTIdentifier()
-    {
-        return $this->id;
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
