7fd4d0c6fbe1da0781db423dc9beba46a10a73a7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-18 16:45:16 +0300

    removing rubbish

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,8 +6,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(RolesTableSeeder::class);
-        $this->call(RegionsTableSeeder::class);
-        $this->call(TechOperationTypesTableSeeder::class);
+        
     }
 }
